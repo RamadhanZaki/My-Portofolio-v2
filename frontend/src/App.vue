b1e56c3016fadb5121ceff167388153dd882e783
RamadhanZaki <amimi1040@gmail.com>
2025-07-13 22:03:17 +0700

    Update Last 8

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
-<script setup> 
-import { RouterView } from 'vue-router'; 
-</script> 
-<template>
-<div class="bg-gradient-to-r from-green-100 via-blue-50 to-blue-100 min-h-screen font-sans">
-  <RouterView />
-</div>
-</template>
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
 
+<template>
+  <div class="bg-white dark:bg-gray-900 text-gray-900 dark:text-white font-sans transition-colors duration-500 min-h-screen">
+    <RouterView />
+  </div>
+</template>

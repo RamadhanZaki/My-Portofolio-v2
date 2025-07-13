d39046e0fdfa9d4112bac5dcd296062e9d2921b1
RamadhanZaki <amimi1040@gmail.com>
2025-07-13 16:13:54 +0700

    Fix axios version, update vite config & vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist' 
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
